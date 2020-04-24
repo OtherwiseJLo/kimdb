588c296b24bd2875a10962c66000bb0396ef1b49
jseym <jseym@10.0.0.75>
2020-04-24 11:54:00 -0400

    Moved Title data class to main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,37 @@
 package imdb
+
 import java.io.File
+
+data class title_akas(val titleId: String,
+					  val ordering: Int,
+					  val title: String,
+					  val region: String,
+					  val language: String,
+					  val types: List<String>,
+					  val attributes: List<String>,
+					  val isOriginalTitle: Boolean);
+
+data class title_basics(val tconst: String,
+						val titleType: String,
+						val primaryTitle: String,
+						val originalTitle: String,
+						val originalTitle: String,
+						val isAdult: Boolean,
+						val startYear: List<String>,
+						val endYear: Boolean,
+						val runtimeMinutes: Int,
+						val genres: List<String>);
 
 fun filePath(fileName: String = ""): String = "/home/jlopez/imdb/$fileName"
 fun fileString(filePath: String): List<String> = File(filePath).readLines()
 
 fun main() {
-    val imdbFiles: FileTreeWalk = File(filePath()).walk()
-    for(f in imdbFiles) {
-        println(f)
-    }
+	val imdbFiles: FileTreeWalk = File(filePath()).walk()
+	for (f in imdbFiles) {
+		println(f)
+	}
+}
+
+fun createDataClases() {
+
 }
